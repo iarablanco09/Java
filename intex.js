652dad82125726c787f9f6301632b07b284017c6
Iara Blanco <iarablanco382gmail.com>
2023-09-11 20:32:18 -0300

    Ultimos arreglos de preeentrega 2

--- intex.js
+++ intex.js
@@ -1,6 +1,4 @@
-
 const hora=prompt("Ingrese la hora (Desde 00 a 23)");
-
 if(hora >= 6 && hora <= 12){
     console.log("Horario diurno")
     alert("¡Buenos dias!");
@@ -13,6 +11,7 @@ if(hora >= 6 && hora <= 12){
 }
 
 let vof=true;
+
 while (vof){
     const edadingresada= Number(prompt("Ingrese su edad"));
     if(edadingresada >=18){
@@ -22,11 +21,12 @@ while (vof){
     }else{
         console.log("Menor de edad");
         alert("Lo sentimos, ingresaste la edad de"+" "+edadingresada+" "+"y para ingresar al sitio debes ser mayor de 18 años.");
+        vof=true;
 
     }
 }
 
-let tof = true;
+let tof=true;
 let nombre = "";
 
 while(tof){
@@ -35,37 +35,41 @@ while(tof){
         console.log("Nombre ingresado correctamente")
        alert("!Bienvenida al sitio web"+" "+nombre+ "!");
     tof=false;
+    break;
 }else{
     alert ("Por favor,complete todos los campos");
     console.log("Error")
-}
-}
+    tof=true;
+}}
 
-let tOrF=true;
-function initEntradas(){
-    while(tOrF){
-        let opSeleccion=parseInt(prompt("Elija una opcion:\n 1-Comprar entradas:$2500 \n 2-Comprar entrada a mesas VIP \n 3-Elegir metodo de pago\n 4-Seguir comprando \n 5-Salir"));
-        switch(opSeleccion){
-        case 1:
-        comprarEntrada();
-        tOrF=true;
-        break;
-        case 2:
-        mesasVipSeleccion();
-        tOrF=true;
-        break;
-        case 3:
-        metodoDePago();
-        tOrF=true;
-        break;
-        case 4:
-        Producto();
-        tOrF=true;
-        break;
-        case 5:
-        tOrF=false;
+let tOrF = true;
+function initEntradas() {
+    while (tOrF) {
+        let opSeleccion = parseInt(prompt("Elija una opción:\n 1-Comprar entradas: $2500 \n 2-Comprar entrada a mesas VIP \n 3-Elegir método de pago\n 4-Seguir comprando \n 5-Salir"));
+        switch (opSeleccion) {
+            case 1:
+                comprarEntrada();
+                break;
+            case 2:
+                mesasVipSeleccion();
+                break;
+            case 3:
+                metodoDePago();
+                break;
+            case 4:
+                comprarProductos();
+                break;
+            case 5:
+                tOrF = false;
+                break;
+            default:
+                alert("Por favor, ingrese una opción válida.");
+                break;
         }
-    }}
+    }
+}
+initEntradas();
+
 function comprarEntrada(){
     let cantSeleccion= parseInt(prompt("Ingrese la cantidad de entradas que quiere comprar."));
     while(tOrF){
@@ -79,7 +83,76 @@ function comprarEntrada(){
         }
     }
 }
+
+
+function mesasVipSeleccion() {
+    while (tOrF) {
+        const mesaVipSeleccion = parseInt(prompt("Elija una mesa:\n(Una vez que termine con su elección, vuelva al inicio para realizar el pago)\n 1-Mesa Green (Max 5 personas): $25000 \n 2-Mesa Blue (Max 10 personas): $50000 \n 3-Mesa Red (Max 20 personas): $100000 \n 4-Salir/Volver a inicio"));
+
+        if (isNaN(mesaVipSeleccion)) {
+            alert("Por favor, ingrese una opción válida.");
+        } else if (mesaVipSeleccion == 4) {
+            break;
+        } else if (mesaVipSeleccion >= 1 && mesaVipSeleccion <= 3) {
+            switch (mesaVipSeleccion) {
+                case 1:
+                    alert("Haz seleccionado la Mesa Green.");
+                    break;
+                case 2:
+                    alert("Haz seleccionado la Mesa Blue.");
+                    break;
+                case 3:
+                    alert("Haz seleccionado la Mesa Red.");
+            break;
+            break;
+        default:
+            tOrF=false;
+            break;
+    }}}}
+
+
+function Producto(nombre, precio){
+    this.nombre= nombre;
+    this.precio= precio;
+    this.ivaPorcentaje = function () {
+        return this.precio *=1.21;
+        }
+}
+
+const arrayProductos = [
+    { codigo: 1, nombre: "Cerveza (Lata)", precio: 800 },
+    { codigo: 2, nombre: "Champagne (Extra brut 750ml)", precio: 3700 },
+    { codigo: 3, nombre: "Vodka (Absolut 700ml)", precio: 5689 },
+    { codigo: 4, nombre: "Gaseosas o aguas", precio: 750 }
+];
+
+const arrayProductosUsuario = [];
+
+function buscarProductoPorCodigo(codigo, productos) {
+    return productos.find(producto => producto.codigo === codigo);
+}
+
+function agregarElementoALaLista(producto, lista) {
+    lista.push(producto);
+}
+
+function comprarProductos() {
+    while (tOrF) {
+        let codigo = parseInt(prompt("Si desea alguna consumición, seleccione entre las siguientes opciones: \n 1-Cerveza (Lata) $800 \n 2-Champagne (Extra brut 750ml) $3700\n 3-Vodka (Absolut 700ml)$5689 \n 4-Gaseosas o aguas $750 \n 5-Salir "));
+        const productoElegido = buscarProductoPorCodigo(codigo, arrayProductos);
+        if (productoElegido != null) {
+            agregarElementoALaLista(productoElegido, arrayProductosUsuario);
+            alert("Producto agregado al carrito");
+        }
+        let continuar = prompt("Ingrese SI si desea seguir comprando. Si desea salir, presione cualquier otra tecla");
+        if (continuar.toLowerCase() !== 'si') {
+            tOrF = false;
+        }
+    }
+}
+
 initEntradas();
+
 const metodosDePago = ["Efectivo", "Tarjeta de Crédito", "Tarjeta de Débito", "Mercado Pago"];
 console.log("Métodos de pago disponibles:");
 for (let i = 0; i < metodosDePago.length; i++) {
@@ -90,104 +163,10 @@ while (true) {
     const opcionDePago = parseInt(prompt("Seleccione un método de pago (1-4):\n 1-Efectivo \n 2-Tarjeta de credito \n 3-Tarjeta de Debito \n 4-Mercado Pago"));
     if (opcionDePago >= 1 && opcionDePago <= 4) {
         metodoSeleccionado = metodosDePago[opcionDePago - 1];
-        alert(`Ha seleccionado ${metodoSeleccionado}`);
+         alert(`Ha seleccionado ${metodoSeleccionado}`); 
+        //Total a pagar: productos.find(producto => producto.precio === precio))//
         break;
     } else {
         alert("Opción incorrecta. Por favor, seleccione un método de pago válido.");
     }
-}
-
-
-function mesasVipSeleccion(){
-    while(tOrF){
-        const mesasVipSeleccion=parseInt(prompt("Elija una mesa:\n(Una vez que termine con su eleccion,vuelva al inicio para realizar pago)\n 1-Mesa Green (Max 5 personas):$25000 \n 2-Mesa Blue (Max 10 personas):$50000 \n 3-Mesa Red (Max 20 personas):$100000 \n 4-Salir/Volver a inicio"));
-
-        if(mesasVipSeleccion==""){
-            alert("Por favor, Ingrese una opcion.")
-        }else if(mesasVipSeleccion==4){
-            break;
-        }
-        switch(mesasVipSeleccion){
-        case 1:
-            alert("Haz seleccionado la Mesa Green.");
-            tOrF=true;
-            break;
-        case 2:
-            alert("Haz seleccionado la Mesa Blue.");
-            tOrF=true;
-            break;
-        case 3:
-            alert("Haz seleccionado la Mesa Red.");
-            tOrF=true;
-            break;
-        default:
-            tOrF=false;
-            break;
-    }}}
-
-function Producto(nombre, precio){
-    this.nombre= nombre;
-    this.precio= precio;
-    this.ivaPorcentaje = function () {
-        return this.precio * 0.21;
-    }
-};
-
-
-let trueOrFalse= true;
-let producto
-
-while (trueOrFalse){
-    let nombre=parseInt(prompt("Si desea alguna consumision, selecciones entre las siguientes: \n 1-Cerveza (Lata) $800 \n 2-Champagne (Extra brut 750ml) $3700\n 3-Vodka (Absolut 700ml)$5689 \n 4-Gaseosas o aguas $750) \n 5-Salir "));
-    let precio =Number(prompt("Ingrese el precio del producto que quiere comprar: \n 1-Cerveza (Lata):$800 \n 2-Champagne (Extra brut 750ml): $3700\n 3-Vodka (Absolut 700ml): $5689 \n 4-Gaseosas o aguas: $750 \n 5-Salir"));
-
-    if(nombre ===""|| precio===""|| precio!==Number){
-        alert("Por favor, complete todos los campos correctamente");
-
-    }else{
-        producto= new Producto(nombre, precio);
-        trueOrFalse=false;
-    }
-
-}
-console.log("precio" in producto);
-for(propiedad in producto){
-console.log(`Para la propiedad ${propiedad} el valor es ${producto[propiedad]}`)
-};
-
-trueOrFalse=true;
-while(trueOrFalse){
-    const opciones= Number(prompt("¡Ultimo paso! A continuacion puede acceder a la informacion de su producto ingresando la opcion 1 o 2: \n 1-Corroborar el producto y su precio \n 2-Ver precio del producto con el iva incluido \n Si esta seguro de que NO quiere ninguna consumision ingrese la opcion numero 3: \n 3-Salir"));
-    switch(opciones){
-        case 1:
-            alert(`El nombre del producto es ${producto.nombre} y su precio es ${producto.precio}`);
-            break;
-        case 2:
-            alert(`El precio del producto es ${producto.precio} y su  iva incluido es ${producto.ivaPorcentaje()} y su precio total es ${producto.precio + producto.ivaPorcentaje()}`)
-            break;
-
-        case 3:
-            trueOrFalse=false;
-            break;
-        default:
-            alert("Opcion incorrecta");
-            break;
-    }}
-
-
-const productos = [
-    { nombre: "Cerveza (Lata)", precio: 800 },
-    { nombre: "Champagne (Extra brut 750ml)", precio: 3700 },
-    { nombre: "Vodka (Absolut 700ml)", precio: 5689 },
-    { nombre: "Gaseosas o aguas", precio: 750 }
-];
-
-// Luego, en el bucle de selección de productos:
-const opcion = parseInt(prompt("Seleccione un producto (1-4): \n 1-Cerveza \n 2-Champagne \n 3-Vodka \n 4-Gaseoda o agua"));
-
-if (opcion >= 1 && opcion <= productos.length) {
-    const productoSeleccionado = productos[opcion - 1];
-    console.log(`Producto seleccionado: ${productoSeleccionado.nombre}, Precio: $${productoSeleccionado.precio}`);
-} else {
-    alert("Opción incorrecta. Por favor, seleccione un producto válido.");
 }
